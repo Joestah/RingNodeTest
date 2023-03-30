7096ece468045eaf84a8195424052be8dabee529
Joesc <jos00220@students.stir.ac.uk>
2023-03-30 18:54:35 +0100

    Added Communication ability to the nodes

--- Node.java
+++ Node.java
@@ -1,8 +1,32 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Node {
 
-    String ipAddress;
-    String port;
+    private String ipAddress;
+    private int port;
+    private int neighbourPort;
+    private boolean hasToken;
+
+    public Node(String ipAddress, int port, int neighbourPort) {
+        this.port = port;
+        this.neighbourPort = neighbourPort;
+        this.ipAddress = ipAddress;
+
+        try {
+            Socket s = new Socket(ipAddress, port);
+            if (s.isConnected()) {
+                System.out.println("Node on port " + port + "is connected" + neighbourPort);
+            } else {
+                System.out.println("Node on port " + port + "failed to connect" + neighbourPort);
 
 
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
 
+    }
 }
+
